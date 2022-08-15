b0290e4521d0415b79e921807e9f4b29236b9576
lugggas <32685131+lugggas@users.noreply.github.com>
2022-08-15 09:42:22 +0200

    Add point free swap implementation

--- haskell/cat4progs/src/Lib.hs
+++ haskell/cat4progs/src/Lib.hs
@@ -1,6 +1,19 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc,
+  )
+where
+
+import Data.Bifunctor (Bifunctor (bimap, first, second))
+import Text.Printf (printf)
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc =
+  let (a, b) = (2, 3)
+   in let c = swap (a, b)
+       in print c
+
+tuple :: (c -> a) -> (c -> b) -> c -> (a, b)
+tuple f g c = (f c, g c)
+
+swap :: (a, b) -> (b, a)
+swap = tuple snd fst
