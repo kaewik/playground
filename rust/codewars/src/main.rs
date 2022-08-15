92c05d39631a614a6237a735045d7d1b68095cdb
lugggas <32685131+lugggas@users.noreply.github.com>
2022-08-15 09:42:18 +0200

    add: rust codewars package

---
+++ rust/codewars/src/main.rs
@@ -0,0 +1,26 @@
+
+
+fn main() {
+    array_diff([1,2,3].to_vec(), [2,2].to_vec());
+}
+
+fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
+    a.into_iter().filter(|elem_of_a| has_not(&b, elem_of_a)).collect()
+}
+
+fn has_not<T: PartialEq>(vec: &Vec<T>, elem: &T) -> bool {
+    vec.iter().find(|&elem_of_vec| *elem_of_vec == *elem).is_none()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn returns_expected() {
+        assert_eq!(array_diff(vec![1,2], vec![1]), vec![2]);
+        assert_eq!(array_diff(vec![1,2,2], vec![1]), vec![2,2]);
+        assert_eq!(array_diff(vec![1,2,2], vec![2]), vec![1]);
+        assert_eq!(array_diff(vec![1,2,2], vec![]), vec![1,2,2]);
+        assert_eq!(array_diff(vec![], vec![1,2]), vec![]);
+    }
+}
