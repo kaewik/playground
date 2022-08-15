7f3d6428b79f2bf4ebfbf59fc82f626229ecac2f
lugggas <32685131+lugggas@users.noreply.github.com>
2022-08-15 09:42:22 +0200

    Add unit assoc and bimap for Either2 pointfree

--- haskell/cat4progs/src/Lib.hs
+++ haskell/cat4progs/src/Lib.hs
@@ -4,16 +4,31 @@ module Lib
 where
 
 import Data.Bifunctor (Bifunctor (bimap, first, second))
-import Text.Printf (printf)
 
 someFunc :: IO ()
 someFunc =
-  let (a, b) = (2, 3)
-   in let c = swap (a, b)
-       in print c
+  do
+    let p1 = (2, 3)
+    let p2 = 12
+    let p3 = (1, (2, 3))
+    print (swap p1)
+    print (unit p2)
+    print (assoc p3)
 
-tuple :: (c -> a) -> (c -> b) -> c -> (a, b)
-tuple f g c = (f c, g c)
+double :: a -> (a, a)
+double a = (a, a)
 
 swap :: (a, b) -> (b, a)
-swap = tuple snd fst
+swap = bimap snd fst . double
+
+unit :: a -> ((), a)
+unit = first (const ()) . double
+
+assoc :: (a, (b, c)) -> ((a, b), c)
+assoc = bimap (second fst) (snd . snd) . double
+
+data Either2 a b = Left2 a | Right2 b
+
+instance Bifunctor Either2 where
+  bimap f g (Left2 a) = (Left2 . f) a
+  bimap f g (Right2 a) = (Right2 . g) a
